e0302e1d28c1e560cfcb2a2b93684fa6e07cd986
DmitryZ <zagarskih@inbox.ru>
2023-04-26 21:32:23 +0500

    p_3 go

--- practice2/app/src/main/java/com/example/practice2/MainActivity.kt
+++ practice2/app/src/main/java/com/example/practice2/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.practice2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val timer = object : CountDownTimer(8000,1000)
+        {
+            override fun onTick(millisUntilFinished: Long){
+
+            }
+            override fun onFinish(){
+                val intent = Intent(this@MainActivity,MainActivity2::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
+        timer.start()
     }
 }
