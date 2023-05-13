1a8ebadce21bf2f49bd4c4fd9baf34df3918382f
DmitryZ <zagarskih@inbox.ru>
2023-05-13 13:25:07 +0500

    practice5 ok

--- practice5/app/src/main/java/com/example/practice5/MainActivity.kt
+++ practice5/app/src/main/java/com/example/practice5/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.practice5
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val poster_res:RecyclerView=findViewById((R.id.rec_view))
+        poster_res.adapter = Poster_adapter(this, PosterList().list)
     }
 }
--- practice5/app/src/main/java/com/example/practice5/Poster.kt
+++ practice5/app/src/main/java/com/example/practice5/Poster.kt
@@ -1,9 +1,18 @@
 package com.example.practice5
 
 data class Poster(val imageId:Int, val title:String)
-class PosterList{val list= arrayListOf(Poster(R.drawable.image1, "Осеннее приключение в приятной компании"),
-    Poster(R.drawable.image2, "Дом, милый дом"),
-    Poster(R.drawable.image3, "Осеннее приключение в приятной компании"),
-    Poster(R.drawable.image4, "Осеннее приключение в приятной компании"),
-    Poster(R.drawable.image5, "Осеннее приключение в приятной компании"))
+class PosterList{val list= arrayListOf(
+    Poster(R.drawable.back, "Назад в будущее"),
+    Poster(R.drawable.cars, "Тачки"),
+    Poster(R.drawable.coco, "Тайна Коко"),
+    Poster(R.drawable.inter, "Интерстеллар"),
+    Poster(R.drawable.ivan, "Иван Васильевич меняет профессию"),
+    Poster(R.drawable.lion, "Король лев"),
+    Poster(R.drawable.lord, "Властелин колец"),
+    Poster(R.drawable.prison, "Побег из Шоушенка"),
+    Poster(R.drawable.pump, "Криминальное чтиво"),
+    Poster(R.drawable.rats, "Рататуй"),
+    Poster(R.drawable.fight, "Бойцовский клуб"),
+    Poster(R.drawable.gm, "Зеленая миля"
+    ))
 }
